a9f18d0d94192037417813f891b8ce93807127c6
Fernando Martinez <martinezfer@gmail.com>
2019-01-19 15:20:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,7 +28,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element) {
 }
 
 function removeElementFromBeginningOfArray(array, element) {
-  array.slice(-element);
+  array.slice(1);
   return array;
 }
 
